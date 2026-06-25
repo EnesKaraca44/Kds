8e451a6a5ca921ac98ed51c55fb37f9cf9146de9
ENES <enes4421@hotmail.com>
2026-06-25 08:59:30 +0300

    Stok detay sorgusunu Rapor API STOK_ALIS_SATIS ile bagla.
    Gomulu detay SQL kaldirildi; rapor_sql_kod_map.json eslemesi eklendi.

--- KDS/flask_app/database/stok_durum_sorgular.py
+++ KDS/flask_app/database/stok_durum_sorgular.py
@@ -172,104 +172,6 @@ def stok_durum_verisi_yukle(start_date_str, end_date_str, birim_id=None, birim_i
         conn.close()
 
 
-# Tek bir stok kartinin hareket detayini (alis/satis fiyat, miktar, fis) doner.
-# Parametreler: {start_date}, {end_date} (YYYY-MM-DD), {butce} (butce tur id), {stok_id} (STOK_ID)
-_STOK_DURUM_DETAY_SQL = """
-select row_number() OVER (order by sfTarih asc) as rowNumber, * from
-(select   b.BIRIM_AD as birimAd, shTarih, belgeAd, stokHareketTip, hareketEnvanterTip,
- stokKod, stokAd, shOlcuBirimAd, shStokAciklama, shMiktar, shKdvOran, shFiyat, shToplam, shKdvMatrah, shKdvNetTutar,shKdvNetTutarDvz, shCikisMlytTutar, shNetSatisTutar,
-sf.STOK_FS_BELGE_NO as sfBelgeNo,
-isnull(ck.CARI_KART_ADI, stokAd) as cariKartAdi,
-isnull(sf.STOK_FS_FATURA_TARIHI,isnull(sf.STOK_FS_IRSALIYE_TARIHI,sf.STOK_FS_FIS_TARIHI)) as sfTarih,
-sf.STOK_FS_FIS_KOD as stokFsFisNo,sf.STOK_FS_FATURA_NO as sfFaturaNo,
-kdvsizShFiyat,shKdvDhAd
- from (select sh.STOK_HAREKET_ID as stokHareketId, sh.SH_BIRIM_ID as shBirimId , sh.SH_STOKKART_ID as shStokKartId, sh.SH_BIRIM_TUR_ID as shBirimTurId,
-  sh.SH_STOK_FIS_ID as shStokFisId, sh.SH_FATURA_ID as shFaturaId, sh.SH_IRSALIYE_ID as shIrsaliyeId, sh.SH_TARIH as shTarih,sht.HAREKET_TUR_AD as belgeAd,
-  sk.STOK_KODU as stokKod, sk.STOK_AD as stokAd, td.TUR_DEGER as shOlcuBirimAd,isnull(sh.SH_STOK_ACIKLAMA, sk.STOK_AD) as shStokAciklama, sh.SH_OZEL_KOD as ozelKod,
-  sh.SH_MIKTAR * isnull(sh.SH_BIRIM_CARPAN,1) / 1 as shMiktar, sh.SH_HAREKET_TUR_ID as shHareketTurId, tdSht.TUR_DEGER_KOD as stokHareketTip,
-  sh.SH_MIKTAR * isnull(sh.SH_BIRIM_CARPAN,1) / 1 - SUM(CASE WHEN sff.STOK_HAREKET_MIKTAR IS NULL THEN 0 ELSE sff.STOK_HAREKET_MIKTAR / 1 END) AS shMevcutMiktar,
-  SUM(CASE WHEN sff.STOK_HAREKET_MIKTAR IS NULL THEN 0 ELSE sff.STOK_HAREKET_MIKTAR / 1 END) AS shCikisMiktar,
-   sh.SH_KDV_DH as shKdvDh,case when sh.SH_KDV_DH = 0 then 'Haric' else 'Dahil' end as shKdvDhAd,sh.SH_KDV_ORAN as shKdvOran,
-  CASE WHEN sht.HAREKET_ENVANTER_TIP = 1 AND sht.HAREKET_FIS_TIP = 4 THEN (sh.SH_KDV_MATRAH + sh.SH_KDV_NET_TUTAR) / sh.SH_MIKTAR ELSE sh.SH_FIYAT END as shFiyat,
-  sh.SH_TOPLAM as shToplam, sh.SH_KDV_MATRAH as shKdvMatrah, sh.SH_KDV_NET_TUTAR as shKdvNetTutar,
-  sh.SH_KDV_NET_TUTAR_DVZ as shKdvNetTutarDvz, sh.SH_CIKIS_MLYT_TUTAR as shCikisMlytTutar, sh.SH_NET_SATIS_TUTAR as shNetSatisTutar,
-  sh.SH_BIRIM_CARPAN as shBirimCarpan, sh.SH_VADE_TARIH as shVadeTarih, sh.SH_VADE_GUN as shVadeGun, sh.SH_ISLEM_SAAT as shIslemSaat,
-  sh.SH_KUNYE_NO as shKunyeNo, sh.SH_BARKOD_NO as shBarkodNo, sh.SH_ACIKLAMA as shAciklama,  sht.HAREKET_ENVANTER_TIP as hareketEnvanterTip,
-  isnull(sh.SH_STOK_FIS_ID, isnull(sh.SH_FATURA_ID, sh.SH_IRSALIYE_ID)) as shOrtakFisId, sht.HAREKET_DEVIR_FISI as hareketDevirFisi,
-  sg.GRUP_KOD as grupKod, sg.GRUP_AD as grupAd, skt.STOK_KART_TIPI_ID as stokKartTipId, skt.KART_ADI as stokKartTipAd, sht.HAREKET_FIS_TIP as hareketFisTip,
-  sh.SH_MKYS_STOK_HAREKET_ID as shMkysStokHareketId, sh.SH_MKYS_STOK_HAREKET_CIKIS_ID as shMkysStokHareketCikisId, sh.SH_MKYS_STOK_HAREKET_DEVIR_ID as shMkysStokHareketDevirId,
-  shh.HASTA_AD_SOYAD as hastaAdSoyad,shh.HASTA_SERVIS_AD as servisAd,shh.HASTA_GELIS_ID as hastaGelisNo,shh.HST_YIL as hstYil,shh.HASTA_YATIS_ID as hastaYatisNoStr,sk.TERAPOTIK_ILAC_TUR_ID as terapotikIlacTurId,
-  sem.ETKEN_MADDE_GEBELIK as etkenMaddeGebelik,sem.ETKEN_MADDE_EMZIRME as etkenMaddeEmzirme,sk.YUKSEK_RISKLI_ILAC as yuksekRiskliIlac,shh.HASTA_YATIS_TRH as hastaYatisTrh,shh.HASTA_GELIS_TRH as hastaGelisTrh,
-  shh.HEKIM_AD_SOYAD as hekimAdSoyad,shh.HASTA_ID as hastaKimlikId,shh.HASTA_SERVIS_ID as hastaServisId,sh.SH_FIYAT as kdvsizShFiyat,
-  sh.SH_DOVIZ_KUR as Dvz_Kur,isnull(sh.SH_DOVIZ_TOPLAM,0) as Doviz_Tutar,pr.PARA_BIRIM_KISA_AD as Para_Birim,isnull(sh.SH_FIYAT_DVZ,0) as Doviz_Birim_Fiyat,sk.ILAC_JENERIK_KODU as jenerikKod
-from STOK_HAREKET sh
-inner join STOK_KART sk with (nolock)
-on sk.STOK_ID = sh.SH_STOKKART_ID and sk.STOK_OZELLIK & 1 = 1  and isnull(sk.PSF_ID,0)= 0
-inner join TUR_DETAY td with (nolock)
-on td.TUR_DETAY_ID = sh.SH_BIRIM_TUR_ID and isnull(td.PSF_ID,0)= 0
-inner join STOK_HAREKET_TUR sht with (nolock)
-on sh.SH_HAREKET_TUR_ID = sht.STOK_HAREKET_TUR_ID  and isnull(sht.PSF_ID,0)= 0
-left join TUR_DETAY tdSht with (nolock)
-on tdSht.TUR_DETAY_ID = sht.HAREKET_TIP and isnull(tdSht.PSF_ID,0)=0
-left join STOK_FIFO sff with (nolock)
-on sff.STOK_HAREKET_GIRIS_ID = sh.STOK_HAREKET_ID and isnull(sff.PSF_ID,0)=0
-left join STOK_GRUP sg with (nolock)
-on sg.STOK_GRUP_ID = sk.STOK_GRUP_ID and isnull(sg.PSF_ID,0) = 0
-left join STOK_KART_TIPI skt with (nolock)
-on skt.STOK_KART_TIPI_ID = sk.STOK_TUR_ID and isnull(skt.PSF_ID,0) = 0
-left join STOK_HAREKET_HASTA as shh with (nolock)
-on shh.STOK_FIS_ID = sh.SH_STOK_FIS_ID and isnull(shh.PSF_ID,0)=0
-left join SKRS_ILAC as si with (nolock)
-on si.BARKODU =  sk.STOK_BARKOD and isnull(si.PSF_ID,0)=0
-left join SBS_ILAC_ETKEN_MADDE as ilacEtken with (nolock)
-on ilacEtken.SKRS_ILAC_ID = si.SKRS_ILAC_ID and isnull(ilacEtken.PSF_ID,0)=0
-left join SKRS_ETKEN_MADDE as sem with (nolock)
-on sem.SKRS_ETKEN_MADDE_ID = ilacEtken.SKRS_ETKEN_MADDE_ID and isnull(sem.PSF_ID,0)=0
-left join PARA_BIRIM  pr with (nolock)
-on pr.PARA_BIRIM_ID = sh.SH_DOVIZ_TUR_ID and isnull(pr.PSF_ID,0)=0
-where isnull(sh.PSF_ID,0)= 0 AND (sh.SH_TARIH >= '{start_date}') AND ( sh.SH_TARIH< DATEADD(day, 1, '{end_date}'))
-AND isnull(sht.HAREKET_FIS_TIP,0) <> 2
- AND sht.HAREKET_ENVANTER_TIP IN (1,2,3)
-group by sh.STOK_HAREKET_ID, sh.SH_BIRIM_ID, sh.SH_STOKKART_ID, sh.SH_BIRIM_TUR_ID, sh.SH_HAREKET_TUR_ID,
-sh.SH_STOK_FIS_ID, sh.SH_FATURA_ID, sh.SH_IRSALIYE_ID, sh.SH_TARIH,sht.HAREKET_TUR_AD,sht.HAREKET_DEVIR_FISI ,
-sk.STOK_KODU, sk.STOK_AD, td.TUR_DEGER,isnull(sh.SH_STOK_ACIKLAMA, sk.STOK_AD), sh.SH_OZEL_KOD, sh.SH_MIKTAR, sht.HAREKET_FIS_TIP,
-sh.SH_KDV_DH , sh.SH_KDV_ORAN, sh.SH_FIYAT,sh.SH_TOPLAM, sh.SH_KDV_MATRAH, sh.SH_KDV_NET_TUTAR, tdSht.TUR_DEGER_KOD,
-sh.SH_KDV_NET_TUTAR_DVZ, sh.SH_CIKIS_MLYT_TUTAR, sh.SH_NET_SATIS_TUTAR, sh.SH_BIRIM_CARPAN, sh.SH_VADE_TARIH, sh.SH_VADE_GUN, sh.SH_ISLEM_SAAT,
-sh.SH_KUNYE_NO, sh.SH_BARKOD_NO, sh.SH_ACIKLAMA, isnull(sh.SH_STOK_FIS_ID, isnull(sh.SH_FATURA_ID, sh.SH_IRSALIYE_ID)),
-sg.GRUP_KOD , sg.GRUP_AD , skt.STOK_KART_TIPI_ID ,skt.KART_ADI, sh.SH_MKYS_STOK_HAREKET_ID, sh.SH_MKYS_STOK_HAREKET_CIKIS_ID, sh.SH_MKYS_STOK_HAREKET_DEVIR_ID,
-sht.HAREKET_ENVANTER_TIP, sht.HAREKET_FIS_TIP,
-shh.HASTA_AD_SOYAD,shh.HASTA_SERVIS_AD,shh.HASTA_GELIS_ID,shh.HST_YIL,shh.HASTA_YATIS_ID,sk.TERAPOTIK_ILAC_TUR_ID,sem.ETKEN_MADDE_GEBELIK,sem.ETKEN_MADDE_EMZIRME ,
-sk.YUKSEK_RISKLI_ILAC,shh.HASTA_YATIS_TRH,shh.HASTA_GELIS_TRH,shh.HEKIM_AD_SOYAD,shh.HASTA_ID,shh.HASTA_SERVIS_ID,
-sh.SH_DOVIZ_KUR ,sh.SH_DOVIZ_TOPLAM ,pr.PARA_BIRIM_KISA_AD,sh.SH_FIYAT_DVZ,sk.ILAC_JENERIK_KODU
-) as tmpSh
-inner join STOK_FIS sf with (nolock)
-on tmpSh.shOrtakFisId = sf.STOK_FS_ID and isnull(sf.PSF_ID,0)=0
-AND convert(date,tmpSh.shTarih) = convert(date,isnull(sf.STOK_FS_MAL_KABUL_TARIHI,sf.STOK_FS_BELGE_TARIHI))
-left join STOK_SEVIYE sv with (nolock)
-on sv.STOK_ID = tmpSh.shStokKartId and sv.SEVIYE_BIRIM_ID = tmpSh.shBirimId  and sv.SEVIYE_BIRIM_TUR_ID = tmpSh.shBirimTurId and sf.STOK_FS_BUTCE_TUR_ID = sv.SEVIYE_BUTCE_TUR_ID and isnull(sv.PSF_ID,0)=0
-left join CARI_KART ck with (nolock)
-on ck.CARI_KART_ID = sf.STOK_FS_CARI_KART_ID and isnull(ck.PSF_ID,0)=0
-left join BIRIM b with (nolock)
-on b.BIRIM_ID = sf.STOK_FS_GIRIS_BIRIM_ID and isnull(b.PSF_ID,0)=0
-where (isnull(null,0)=0 or (tmpSh.shBirimId = null))
-and (isnull({butce},0)=0 or sf.STOK_FS_BUTCE_TUR_ID = {butce})
-and (isnull({stok_id},0)=0 or (tmpSh.shStokKartId IN (select Data from dbo.TextToInConditionConvert('{stok_id}',','))))
-and (isnull('HAREKETSATINALMA,HAREKETSATIS,HAREKETSTOK,HAREKETECZANE','')='' or (tmpSh.stokHareketTip IN (select Data from dbo.TextToInConditionConvert('HAREKETSATINALMA,HAREKETSATIS,HAREKETSTOK,HAREKETECZANE',','))))
-group by stokHareketId, shBirimId, shStokKartId, shBirimTurId, shHareketTurId,shStokFisId, shFaturaId, shIrsaliyeId, shTarih, belgeAd, stokHareketTip, b.BIRIM_AD, hareketFisTip,
-       stokKod, stokAd, shOlcuBirimAd, shStokAciklama, ozelKod, shMiktar, shMevcutMiktar,shCikisMiktar, shKdvDh, shKdvOran, shFiyat, shToplam, shKdvMatrah, shKdvNetTutar,
-       shKdvNetTutarDvz, shCikisMlytTutar, shNetSatisTutar, shBirimCarpan, shVadeTarih, shVadeGun, shIslemSaat, sf.STOK_FS_FATURA_NO,sf.STOK_FS_BELGE_NO ,
-shKunyeNo, shBarkodNo, shAciklama, shOrtakFisId, grupKod, grupAd, stokKartTipId,stokKartTipAd, sf.STOK_FS_IRSALIYE_TARIHI,sf.STOK_FS_FIS_TARIHI, sf.STOK_FS_TOPLAM_INDIRIM,
-       shMkysStokHareketId, shMkysStokHareketCikisId, shMkysStokHareketDevirId, sf.STOK_FS_BUTCE_TUR_ID , sf.STOK_FS_IRSALIYE_NO, hareketEnvanterTip, hareketDevirFisi,
-       sv.STOK_SEVIYE_KRITIK, sv.STOK_SEVIYE_ASGARI ,sv.STOK_SEVIYE_AZAMI, sv.STOK_SEVIYE_YILLIK,ck.CARI_KART_KODU,ck.CARI_KART_ADI,sf.STOK_FS_FATURA_TARIHI,
-       sf.STOK_FS_IHALE_NO, sf.STOK_FS_IHALE_TARIHI,hastaAdSoyad,servisAd,hastaGelisNo,hstYil,hastaYatisNoStr,terapotikIlacTurId,etkenMaddeGebelik,etkenMaddeEmzirme,yuksekRiskliIlac,
-       hastaYatisTrh,hastaGelisTrh,hekimAdSoyad,hastaKimlikId,hastaServisId,sf.STOK_FS_DAYANAK_NO,sf.STOK_FS_FIS_KOD,sf.STOK_FS_ISLEM_YAPAN_KIMLIK_ID,
-       sf.STOK_FS_IHALE_NO,sf.STOK_FS_IHALE_TARIHI,kdvsizShFiyat,shKdvDhAd,
-       Dvz_Kur, Doviz_Tutar,Para_Birim,Doviz_Birim_Fiyat
-having (isnull(null,0) =0 or isnull(null,0) =0 or (dbo.numericConditionCheck(sum(convert(decimal(27,13),shMevcutMiktar)), null,null) = 1))
-) as durumCount
-"""
-
-
 def _detay_satir_tipi(hareket_tip, envanter_tip, belge_ad) -> str:
     """
     Alis/satis/diger ayrimi:
@@ -334,6 +236,17 @@ def _detay_tarih(val) -> str:
     return str(val).strip()
 
 
+def _fix_stok_alis_satis_sql(sql: str) -> str:
+    """Rapor API'ye kaydedilen detay SQL'ini calistirilabilir forma getirir."""
+    if not isinstance(sql, str) or not sql.strip():
+        return sql
+
+    fixed = sql.strip().rstrip(";").strip()
+    if fixed.startswith("(") and fixed.endswith(")") and fixed[1:].lstrip().lower().startswith("select"):
+        fixed = fixed[1:-1].strip()
+    return fixed
+
+
 @ttl_cache(maxsize=128, ttl=60)
 def stok_durum_detay_yukle(stok_kart_id, start_date_str, end_date_str, butce_tur_id=831):
     """Tek bir stok kartinin hareket detayini doner: ozet + satirlar."""
@@ -356,12 +269,25 @@ def stok_durum_detay_yukle(stok_kart_id, start_date_str, end_date_str, butce_tur
         return bos
 
     try:
-        sql = _STOK_DURUM_DETAY_SQL.format(
-            start_date=start_date_str,
-            end_date=end_date_str,
-            butce=butce,
-            stok_id=sid,
+        sql = get_remote_sql(
+            "stok_durum.stok_durum_detay_yukle",
+            {
+                "start_date": start_date_str,
+                "end_date": end_date_str,
+                "TARIH1": start_date_str,
+                "TARIH2": end_date_str,
+                "butce": butce,
+                "BUTCE": butce,
+                "stok_id": sid,
+                "STOK_ID": sid,
+                "BIRIM_ID": "",
+            },
         )
+        if not sql:
+            print("[STOK_DURUM_DETAY] stok_durum.stok_durum_detay_yukle -> STOK_ALIS_SATIS SQL metni alinamadi")
+            return bos
+
+        sql = _fix_stok_alis_satis_sql(sql)
         df = pd.read_sql(sql, conn)
         df = _normalize_stok_durum_columns(df)
 
